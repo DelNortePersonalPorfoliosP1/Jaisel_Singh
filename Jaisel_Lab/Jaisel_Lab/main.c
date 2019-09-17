18898c870a5d05bd2ecb637d90829065a21342e2
Jaisel singh <singhjaisel01@gmail.com>
2019-09-17 09:55:28 -0700

    for points

---
+++ Jaisel_Lab/Jaisel_Lab/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Jaisel_Lab
+//
+//  Created by Singh, Jaisel on 9/17/19.
+//  Copyright © 2019 labs. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
